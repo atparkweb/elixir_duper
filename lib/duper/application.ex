7d3c35f46f8e53f544665914665216f9418e0473
Andy Park <atpark333@gmail.com>
2019-12-09 20:49:59 +0900

    Add child processes to application

--- lib/duper/application.ex
+++ lib/duper/application.ex
@@ -1,18 +1,19 @@
 defmodule Duper.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      Duper.Results
+      Duper.Results,
+      { Duper.PathFinder, "." },
+      Duper.WorkerSupervisor,
+      { Duper.Gatherer, 1 }
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Duper.Supervisor]
+    # If any of the above children fails that means the whole
+    # application should fail, so :one_for_all is used
+    opts = [strategy: :one_for_all, name: Duper.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
